4106ed4a132dcf49ca4cf734fc9ce9218a61ece8
Haoyan Huo <hhaoyann@gmail.com>
2020-04-04 11:30:39 -0700

    Add footer.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import {
   Sorting,
   WithSearch,
 } from "@elastic/react-search-ui";
-import { Layout, MultiCheckboxFacet } from "@elastic/react-search-ui-views";
+import {Layout, MultiCheckboxFacet} from "@elastic/react-search-ui-views";
 
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 import "./assets/bulma-helpers.css";
@@ -35,7 +35,7 @@ import logo from './covidscholar_logo_cascade.png';
 
 // import { TwitterTimelineEmbed } from "react-twitter-embed";
 
-const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
+const {hostIdentifier, searchKey, endpointBase, engineName} = getConfig();
 const connector = new AppSearchAPIConnector({
   searchKey,
   engineName,
@@ -56,8 +56,8 @@ const config = {
 export default function App() {
   return (
     <SearchProvider config={config}>
-      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-        {({ wasSearched }) => {
+      <WithSearch mapContextToProps={({wasSearched}) => ({wasSearched})}>
+        {({wasSearched}) => {
           return (
             <div className="App">
               <ErrorBoundary>
@@ -66,10 +66,10 @@ export default function App() {
                     <SearchBox
                       autocompleteSuggestions={true}
                       inputView={({
-                        getAutocomplete,
-                        getInputProps,
-                        getButtonProps,
-                      }) => (
+                                    getAutocomplete,
+                                    getInputProps,
+                                    getButtonProps,
+                                  }) => (
                         <>
                           <div className="is-quarter-width">
                             <img className="covid-scholar-logo" src={logo}/>
@@ -79,7 +79,7 @@ export default function App() {
                               {...getInputProps({
                                 placeholder: "Search",
                                 className: "input is-medium is-rounded",
-                                style: { inputmode: "search" },
+                                style: {inputmode: "search"},
                                 autoFocus: true
                               })}
                             />
@@ -123,12 +123,30 @@ export default function App() {
                   }
                   bodyHeader={
                     <React.Fragment>
-                      {wasSearched && <PagingInfo />}
-                      {wasSearched && <ResultsPerPage />}
+                      {wasSearched && <PagingInfo/>}
+                      {wasSearched && <ResultsPerPage/>}
                     </React.Fragment>
                   }
-                  bodyFooter={<Paging />}
+                  bodyFooter={<Paging/>}
                 />
+                <div id="footer_container" className="content has-text-centered sui-layout-body">
+                  <div className="columns is-centered">
+                    <div className="column is-half is-size-6 "><p>This website uses natural language processing (NLP) to
+                      power search on a set of research papers related to COVID-19. It was created by the team behind <a
+                        href="https://www.matscholar.com">Matscholar</a>, a research effort led by the <a
+                        href="https://hackingmaterials.lbl.gov">HackingMaterials</a>, <a
+                        href="https://perssongroup.lbl.gov">Persson</a>, and <a
+                        href="https://ceder.berkeley.edu">Ceder</a> research groups at Lawrence Berkeley National Lab.
+                      The virus icon in our logo was made by Freepik from www.flaticon.com</p></div>
+                  </div>
+                  <div><a href="https://github.com/materialsintelligence/matscholar-web" target="_blank"
+                          className="has-text-weight-bold">About Matscholar</a><span> | </span><a
+                    href="https://www.iubenda.com/privacy-policy/55585319" target="_blank"
+                    className="has-text-weight-bold">Privacy Policy</a><span> | </span><a
+                    href="https://discuss.matsci.org/c/matscholar" target="_blank" className="has-text-weight-bold">Matscholar
+                    Forum</a></div>
+                  <div><span>Copyright © 2019 - Materials Intelligence</span></div>
+                </div>
               </ErrorBoundary>
             </div>
           );
