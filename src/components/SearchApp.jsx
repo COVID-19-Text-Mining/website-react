a7e759a4e9adfaf17ee11867dfaa69c4a3d81a46
Haoyan Huo <hhaoyann@gmail.com>
2020-04-14 10:26:28 -0700

    Add new logo.

--- src/components/SearchApp.jsx
+++ src/components/SearchApp.jsx
@@ -50,85 +50,85 @@ const config = {
 
 export default function SearchApp() {
   return (
-      <SearchProvider config={config}>
-        <WithSearch
-            mapContextToProps={({wasSearched, isLoading}) => ({
-              wasSearched,
-              isLoading,
-            })}>
-          {({wasSearched, isLoading}) => {
-            return (
-                <div className="App">
-                  <ErrorBoundary>
-                    <Layout
-                        header={
-                          <Sticky topOffset={56}>{({style}) =>
-                              <nav className={"nav search-nav"} style={style}>
-                                <Navbar.Brand href="/" className={"text-center"}>
-                                  <Image
-                                      src={logo}
-                                      className="d-inline-block align-top"
-                                      // alt="Covid Scholar logo"
-                                  />
-                                </Navbar.Brand>
-                                <div className={"nav-search-bar"}>
-                                  <SearchBox
-                                      autocompleteSuggestions={true}
-                                      inputView={({
-                                                    getAutocomplete,
-                                                    getInputProps,
-                                                    // getButtonProps,
-                                                  }) => (
-                                          <div className="sui-search-box__wrapper column">
+    <SearchProvider config={config}>
+      <WithSearch
+        mapContextToProps={({wasSearched, isLoading}) => ({
+          wasSearched,
+          isLoading,
+        })}>
+        {({wasSearched, isLoading}) => {
+          return (
+            <div className="App">
+              <ErrorBoundary>
+                <Layout
+                  header={
+                    <Sticky topOffset={56}>{({style}) =>
+                      <nav className={"nav search-nav"} style={style}>
+                        <Navbar.Brand href="/" className={"text-center"}>
+                          <Image
+                            src={logo}
+                            className="d-inline-block align-top"
+                            // alt="Covid Scholar logo"
+                          />
+                        </Navbar.Brand>
+                        <div className={"nav-search-bar"}>
+                          <SearchBox
+                            autocompleteSuggestions={true}
+                            inputView={({
+                                          getAutocomplete,
+                                          getInputProps,
+                                          // getButtonProps,
+                                        }) => (
+                              <div className="sui-search-box__wrapper column">
                               <span className="pl-1 pr-5 search-help-text">
                                 {" "}
                                 Search more than 44,000 COVID-19 papers
                               </span>
-                                            <input
-                                                {...getInputProps({
-                                                  placeholder: "Search",
-                                                  className: "form-control form-rounded m-0",
-                                                  style: {inputmode: "search"},
-                                                  autoFocus: true,
-                                                })}
-                                            />
-                                            {getAutocomplete({
-                                              className: "zindex-popover m-0"
-                                            })}
-                                          </div>
-                                      )}
-                                  />
-                                </div>
-                              </nav>}
-                          </Sticky>
-                        }
-                        sideContent={FilterSideBar(wasSearched)}
-                        bodyContent={
-                          <div>
-                            {isLoading && <div>Loading...</div>}
-                            {!isLoading && (
-                                <Results
-                                    titleField={getConfig().titleField}
-                                    urlField={getConfig().urlField}
-                                    shouldTrackClickThrough={true}
-                                    resultView={ResultView}
+                                <input
+                                  {...getInputProps({
+                                    placeholder: "Search",
+                                    className: "form-control form-rounded m-0",
+                                    style: {inputmode: "search"},
+                                    autoFocus: true,
+                                  })}
                                 />
+                                {getAutocomplete({
+                                  className: "zindex-popover m-0"
+                                })}
+                              </div>
                             )}
-                          </div>
-                        }
-                        bodyHeader={
-                          <React.Fragment>
-                            {wasSearched && <PagingInfo/>}
-                            {wasSearched && <ResultsPerPage/>}
-                          </React.Fragment>
-                        }
-                        bodyFooter={!isLoading && <Paging totalPages={20}/>}
-                    />
-                  </ErrorBoundary>
-                </div>
-            );
-          }}
-        </WithSearch>
-      </SearchProvider>
+                          />
+                        </div>
+                      </nav>}
+                    </Sticky>
+                  }
+                  sideContent={FilterSideBar(wasSearched)}
+                  bodyContent={
+                    <div>
+                      {isLoading && <div>Loading...</div>}
+                      {!isLoading && (
+                        <Results
+                          titleField={getConfig().titleField}
+                          urlField={getConfig().urlField}
+                          shouldTrackClickThrough={true}
+                          resultView={ResultView}
+                        />
+                      )}
+                    </div>
+                  }
+                  bodyHeader={
+                    <React.Fragment>
+                      {wasSearched && <PagingInfo/>}
+                      {wasSearched && <ResultsPerPage/>}
+                    </React.Fragment>
+                  }
+                  bodyFooter={!isLoading && <Paging totalPages={20}/>}
+                />
+              </ErrorBoundary>
+            </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
   );
 }
